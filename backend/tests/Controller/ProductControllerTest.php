af1082a23df32cc9df7f66a7f448f90b76c06849
Denilson21R <45276744+Denilson21R@users.noreply.github.com>
2022-12-28 14:47:58 -0300

    initial tests of product

--- backend/tests/Controller/ProductControllerTest.php
+++ backend/tests/Controller/ProductControllerTest.php
@@ -2,13 +2,87 @@
 
 namespace Tests\Controller;
 
+use App\Models\Product;
+use Illuminate\Support\Env;
 use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
 {
-    //TODO: implement tests of product
-    public function testExample()
+    /*
+     * Perform login with the user and return the token in a prepared string to authenticate.
+     * Login and password of users can be changed in env file.
+     */
+    protected function authenticate(): string
     {
-        $this->assertTrue(true);
+        $email = Env::get('TESTS_LOGIN');
+        $password = Env::get('TESTS_PASSWORD');
+
+        $params_request_login = [
+            "login" => $email,
+            "password" => $password
+        ];
+
+        $result = $this->post('/api/user/auth', $params_request_login);
+
+        return "bearer " . $result->response->json()["api_key"];
+    }
+
+    public function testCanGetAllProducts()
+    {
+        //prepare
+        $token_jwt = $this->authenticate();
+
+        //act
+        $products = $this->get("/api/product", ['Authorization' => $token_jwt]);
+
+        //assert
+        $products->assertResponseStatus(200);
+        $products->seeJsonStructure(
+            [
+                'status',
+                'data'
+            ]
+        );
+    }
+
+    public function testCanAddProduct()
+    {
+        //prepare
+        $token_jwt = $this->authenticate();
+
+        $product = Product::factory()->make();
+
+        $params_request_add_product = [
+            "name" => $product->name,
+            "description" => $product->description,
+            "value" => $product->value
+        ];
+
+        //act
+        $result = $this->post("/api/product", $params_request_add_product, ['Authorization' => $token_jwt]);
+
+        //assert
+        $result->assertResponseStatus(201);
+        $this->seeInDatabase('product', $product->getAttributes());
+    }
+
+    public function testCanGetProductById()
+    {
+        //prepare
+        $token_jwt = $this->authenticate();
+
+        $product = Product::factory()->create();
+
+        //act
+        $product = $this->get("/api/product/".$product->id, ['Authorization' => $token_jwt]);
+
+        //assert
+        $product->assertResponseStatus(200);
+        $product->seeJsonStructure(
+            [
+                'status',
+                'data'
+            ]
+        );
     }
 }
